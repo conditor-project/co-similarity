b51ab15263bdfa650dc8d7d7c8b2e2964a0b599b
Jean-Baptiste Genicot <jean-baptiste.genicot@inist.fr>
2018-05-15 10:43:15 +0200

    mise en place code fonctionnelle

--- index.js
+++ index.js
@@ -1,59 +1,128 @@
-'use strict'
+'use strict';
 
-const _ = require('lodash'),
-  metadataMappings = require('co-config/metadata-mappings.json');
+const es = require('elasticsearch'),
+      esConf = require('co-config/es.js'),
+      _ = require('lodash'),
+      metadata = require('co-config/mapping-shingles.json'),
+      baseRequest = require('co-config/base_request.json'),
+      debug = require('debug')('co-deduplicate'),
+      Promise = require('bluebird');
 
+const esClient = new es.Client({
+    host: esConf.host,
+    log: {
+        type: 'file',
+        level: ['error']
+    }
+});
 
-class CoUnique{
+class CoSimilarity{
 
+  constructor() {
 
-constructor() {
+    this.CONDITOR_SESSION = process.env.ISTEX_SESSION || "TEST_1970-01-01-00-00-00";
+    this.MODULEROOT = process.env.MODULEROOT || __dirname;
 
-  this.listId =[];
-  this.CONDITOR_SESSION = process.env.ISTEX_SESSION || "TEST_1970-01-01-00-00-00";
-  this.MODULEROOT = process.env.MODULEROOT || __dirname;
+  }
 
-}
+  getRequest(docObject){
+    return Promise.try(()=>{
+      let query='';
+      let shingleField='';
+      let mapping=_.get(metadata,'mappings.record.properties',{});
+      _.mapKeys(mapping,(value,key)=>{
+        if (_.get(value,'copy_to','')!=='') {
+          shingleField+=_.get(docObject,key,'');
+          //shingleField+=_.get(docObject,key+'.value','');
+        }
+      });
 
+      query={"query":
+              {"bool":
+                {"must":
+                  {"match":
+                    {"fingerprint":shingleField}
+                  }
+                }
+              }
+            };
 
+      return query;
+    });
+  }
 
-  doTheJob(jsonLine, cb) {
+  getScore(docObject,request){
+    let response = esClient.search({
+      index:esConf.index,
+      body:request
+    });
+    return response;
+  }
 
-    let source = jsonLine.source;
-    let idSource;
-    let nameId;
+  getSimilarity(docObject){
+    return this.getRequest(docObject).then(this.getScore.bind(this,docObject));
+  }
 
-    _.each(metadataMappings,(mappingSource)=>{
-      if (mappingSource.source === source) { nameId = mappingSource.nameID; }
+  insertScore(docObject,result){
+
+    let maxScore = result.hits.max_score;
+    docObject.maxScore = maxScore
+    let minLimit = maxScore*90/100;
+    docObject.minLimit = minLimit;
+
+    let recordId;
+    _.each(result.hits.hits,(hit)=>{
+      if (hit._source.idConditor===docObject.idConditor){
+        recordId = hit._id;
+      }
     });
 
-    if ( nameId === undefined || nameId.trim ==="") {
+    let arrayNearDuplicate = _.map(result.hits.hits,(hit)=>{
+      if ((hit._score>=minLimit || hit.score>200) && hit._source.idConditor!==docObject.idConditor && _.union(hit._source.typeConditor,docObject.typeConditor).length>0){
+        return {
+          score : hit._score,
+          idConditor : hit._source.idConditor,
+        }
+      }
+    });
 
-      let error = {
-        errCode: 1,
-        errMessage: "Aucun mapping valide trouvé pour cette source."
-      };
-      jsonLine.error = error;
-      cb(error);
+    arrayNearDuplicate = _.compact(arrayNearDuplicate);
 
-    }
-    else {
-      idSource = jsonLine[nameId];
-      if (_.indexOf(this.listId,idSource.value) !== -1){
+    docObject.nearDuplicate = arrayNearDuplicate;
 
-        let error = {
-          errCode: 2,
-          errMessage: "ID source déjà présent dans le corpus"
+    return esClient.update({
+      index:esConf.index,
+      type:esConf.type,
+      id:recordId,
+      body:{
+        doc:{
+          nearDuplicate:arrayNearDuplicate
+        }
+      }
+    });
+  }
+
+  doTheJob(docObject, cb) {
+    let error;
+
+    this.getSimilarity(docObject)
+    .then(this.insertScore.bind(this,docObject))
+    .catch(function(e){
+        error = {
+            errCode: 3,
+            errMessage: 'erreur de récupération de similarités : ' + e
         };
-        jsonLine.error = error;
+        docObject.error = error;
         cb(error);
+    })
+    .then(()=>{
+      cb();
+    })
+  }
 
-      }
-      else {
-        this.listId.push(idSource.value);
-        cb();
-      }
-    }
+  finalJob(docObject,cb){
+    cb();
   }
 }
-module.exports = new CoUnique();
+
+module.exports = new CoSimilarity();
