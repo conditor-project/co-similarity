8fbcd27ca59875ef12bfc1b39ac7af26cce349a3
niederlec <claude.niederlender@gmail.com>
2018-09-12 10:48:41 +0200

    don't try to unzip in production mode

--- test/bin/unzip-dataset.sh
+++ test/bin/unzip-dataset.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ls test/dataset/in
-if [ ! -f test/dataset/in/data.json ] || [ ! -f test/dataset/in/test.json ]; then
-    read -s -p "saisissez le mot de passe nécessaire à l'extraction des données de test : " ZIP_PASSWORD
-    unzip -o -P ${ZIP_PASSWORD} ./test/dataset/in/testdata.zip -d ./test/dataset/in
+if [ "$NODE_ENV" != 'production' ]; then
+  if [ ! -f test/dataset/in/data.json ] || [ ! -f test/dataset/in/test.json ]; then
+      read -s -p "saisissez le mot de passe nécessaire à l'extraction des données de test : " ZIP_PASSWORD
+      unzip -o -P ${ZIP_PASSWORD} ./test/dataset/in/testdata.zip -d ./test/dataset/in
+  fi
 fi
