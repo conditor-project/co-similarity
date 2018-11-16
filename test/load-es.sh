2eb773ae37c440859f05067ecfa8a5c19965a906
Rémy MEJA <remymeja@free.fr>
2018-11-16 14:20:22 +0100

    oh come on !

--- test/load-es.sh
+++ test/load-es.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # echo "elasticdump --input=\"dataset/in/settings.json\" --output=\"$1/$2\" --type=\"settings\""
-npx elasticdump --input="dataset/in/settings.json" --output="http://$1/$2" --type="settings"
-npx elasticdump --input="dataset/in/analyzer.json" --output="http://$1/$2" --type="analyzer"
-npx elasticdump --input="dataset/in/mapping.json" --output="http://$1/$2" --type="mapping"
-npx elasticdump --input="dataset/in/data.json" --output="http://$1/$2" --type="data"
+npx elasticdump --input="dataset/in/settings.json" --output="$1/$2" --type="settings"
+npx elasticdump --input="dataset/in/analyzer.json" --output="$1/$2" --type="analyzer"
+npx elasticdump --input="dataset/in/mapping.json" --output="$1/$2" --type="mapping"
+npx elasticdump --input="dataset/in/data.json" --output="$1/$2" --type="data"
