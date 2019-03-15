a02a538ff4cd943ae597b94870abbdbb0eaa379d
Rémy MEJA <remymeja@free.fr>
2019-03-15 10:59:27 +0100

    clean bash script

--- test/bin/unzip-dataset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ls test/dataset/in
-if [ "$NODE_ENV" != 'production' ]; then
-  if [ ! -f test/dataset/in/data.json ] || [ ! -f test/dataset/in/test.json ]; then
-      read -s -p "saisissez le mot de passe nécessaire à l'extraction des données de test : " ZIP_PASSWORD
-      unzip -o -P ${ZIP_PASSWORD} ./test/dataset/in/testdata.zip -d ./test/dataset/in
-  fi
-fi
